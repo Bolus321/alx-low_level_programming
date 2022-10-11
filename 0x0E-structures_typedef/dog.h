cddd4561c40cae5fcbd0418938336b690a2731f5
Bolus321 <nnamdiogbolu321@gmail.com>
2022-10-10 20:44:19 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,47 +1,20 @@
-Lordwill1
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x0E-structures_typedef/dog.h
-@Lordwill1
-Lordwill1 Added tasks on project 0x0E C
- 1 contributor
-31 lines (27 sloc)  547 Bytes
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
- *
- * Description: Longer description
- */
+ * struct dog - user defined data type
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog 
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - typedef for struct dog
- */
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
 #endif
